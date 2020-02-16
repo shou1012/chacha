fcfd5317a015312ddb0d1a5408b6d47a50c876e3
ENDOTAKUMI <en0605taku@gmail.com>
2020-02-16 14:47:27 +0900

    Added back button.

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    var pagetop = $('#pagetop');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
